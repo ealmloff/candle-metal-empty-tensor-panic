6c64e87d27ac2008a22333e6c59857383b0fef6c
Evan Almloff <evanalmloff@gmail.com>
2025-01-12 18:25:46 -0600

    remove cargo.toml from main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-// [package]
-// name = "candle-panic"
-// version = "0.1.0"
-// edition = "2021"
-
-// [dependencies]
-// candle-core = { version = "0.8.2", features = ["metal"] }
-// safetensors = "0.4.0"
-
 use candle_core::{Device, Tensor};
 
 fn main() {
